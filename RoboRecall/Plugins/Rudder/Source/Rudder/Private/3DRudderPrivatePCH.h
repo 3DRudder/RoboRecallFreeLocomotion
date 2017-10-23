7113ac9f51ba89433bd044410cf25cff2c86474b
Unknown <mvivant@3drudder.com>
2017-10-23 10:57:37 +0200

    Clean

--- RoboRecall/Plugins/Rudder/Source/Rudder/Private/3DRudderPrivatePCH.h
+++
@@ -1,7 +0,0 @@
-// You should place include statements to your module's private header files here.  You only need to
-// add includes for headers that are used in most of your module's source files though.
- 
-#include "Core.h"
-#include "CoreUObject.h"
- 
-#include "I3DRudderPlugin.h"
